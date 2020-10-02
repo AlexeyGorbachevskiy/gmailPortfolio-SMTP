21c7d8c382f45dd583435006c87dbdb895235dea
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-10-02 23:40:30 +0300

    fix: cors policy (5)

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const smtp_login = process.env.SMTP_LOGIN || '';
 const smtp_password = process.env.SMTP_PASSWORD || '';
 
 
-app.use(cors({origin: true}));
-app.options('*', cors())
+app.use(cors({origin: '*', optionsSuccessStatus: 200,}));
+// app.options('*', cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -26,17 +26,17 @@ let transporter = nodemailer.createTransport({
     },
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/sendMessage', async function (req, res) {
+app.post('/sendMessage', cors(), async function (req, res) {
 
     const {name, email, message} = req.body;
 
