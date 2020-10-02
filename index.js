66f6c11816d118a1d376639dab4d26cefab8e02b
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-10-02 23:50:35 +0300

    fix: cors policy (7)

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const smtp_password = process.env.SMTP_PASSWORD || '';
 // app.options('*', cors())
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
